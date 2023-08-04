f9356583c5f105e51b9b43aed87398a05f4cccc4
anubra266 <anubra266@gmail.com>
2023-08-04 23:10:52 +0100

    chore: update content

--- app/components/SanityCode.tsx
+++ app/components/SanityCode.tsx
@@ -1,10 +1,12 @@
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
 import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
 import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
-import dark from 'react-syntax-highlighter/dist/cjs/styles/prism/twilight'
+import dark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark'
 
 SyntaxHighlighter.registerLanguage('javascript', js)
 SyntaxHighlighter.registerLanguage('jsx', jsx)
+SyntaxHighlighter.registerLanguage('css', css)
 
 type SanityCodeProps = {
   value: {
--- app/components/SanityContent.tsx
+++ app/components/SanityContent.tsx
@@ -14,6 +14,23 @@ const components = {
     image: SanityImage,
     code: SanityCode,
   },
+  marks: {
+    internalLink: ({ value, children }: any) => {
+      const { slug = {} } = value
+      const href = `/${slug.current}`
+      return <a href={href}>{children}</a>
+    },
+    link: ({ value, children }: any) => {
+      const { blank, href } = value
+      return blank ? (
+        <a href={href} target="_blank" rel="noreferrer">
+          {children}
+        </a>
+      ) : (
+        <a href={href}>{children}</a>
+      )
+    },
+  },
 }
 
 export function SanityContent(props: ContentProps) {
